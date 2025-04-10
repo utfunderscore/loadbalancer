b2eb6e01dc4a065fc9b83eb5ce29a725437689a3
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-04-10 17:15:18 +0100

    fix: Switch away from lateinit and use mutable null variables

--- src/main/kotlin/org/readutf/loadbalancer/client/Client.kt
+++ src/main/kotlin/org/readutf/loadbalancer/client/Client.kt
@@ -1,8 +1,13 @@
 package org.readutf.loadbalancer.client
 
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import net.kyori.adventure.text.Component
 import net.minestom.scratch.network.NetworkContext
 import net.minestom.server.network.packet.client.handshake.ClientHandshakePacket
 import net.minestom.server.network.packet.server.ServerPacket
+import net.minestom.server.network.packet.server.common.DisconnectPacket
 import net.minestom.server.network.player.GameProfile
 import java.nio.channels.SocketChannel
 import java.util.concurrent.atomic.AtomicBoolean
@@ -11,19 +16,29 @@ class Client(
     val socketChannel: SocketChannel,
     val networkContext: NetworkContext.Async = NetworkContext.Async(),
 ) {
-    val start = System.nanoTime()
-
+    var writerThread: Thread? = null
+    var readerThread: Thread? = null
+    var gameProfile: GameProfile? = null
+    var handshakeInfo: ClientHandshakePacket? = null
     val connected = AtomicBoolean(true)
-    lateinit var writerThread: Thread
-    lateinit var readerThread: Thread
-    lateinit var gameProfile: GameProfile
-
-    lateinit var handshakeInfo: ClientHandshakePacket
 
     fun sendPacket(serverPacket: ServerPacket) {
         networkContext.write(serverPacket)
     }
 
-    fun onDisconnect() {
+    fun disconnect(message: Component) {
+        sendPacket(DisconnectPacket(message))
+    }
+
+    fun toPlayer(): Result<Player, Throwable> {
+        val gameProfile = gameProfile ?: return Err(IllegalStateException("Game profile is not set"))
+        val handshakeInfo = handshakeInfo ?: return Err(IllegalStateException("Handshake info is not set"))
+        val clientSettings =
+            ClientSettings(
+                protocolVersion = handshakeInfo.protocolVersion,
+                serverAddress = handshakeInfo.serverAddress,
+                serverPort = handshakeInfo.serverPort,
+            )
+        return Ok(Player(this, gameProfile, clientSettings))
     }
 }
